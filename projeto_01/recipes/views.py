c0053962ca0d72439caf83b7b29567fb19ec7ee3
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-11 11:40:02 -0300

    [feat] adiciona rota dinamica com id tipo int

--- projeto_01/recipes/views.py
+++ projeto_01/recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Anderson C Rodrigues'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Anderson C Rodrigues'
+    })
