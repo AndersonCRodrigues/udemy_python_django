2b760227204ae14daf25f4f182e5fa0063a366c1
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-17 15:21:16 -0300

    [feat] habilita html na descrição da receita <safe>

--- projeto_01/recipes/views.py
+++ projeto_01/recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 
@@ -24,7 +23,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
